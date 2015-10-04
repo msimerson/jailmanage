63c123b84d4fa9c7dfb424bede9e758a7a0c592e
Matt Simerson <matt@tnpi.net>
2015-10-04 14:28:34 -0700

    update to 2013

--- jail_manage.sh
+++ jail_manage.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 #
 # by Matt Simerson
-#   Sep 27, 2007 - added all target
-#   Sep 23, 2007 - added tripwire
-#   Sep 18, 2007 - added sudo
-#   Sep 16, 2007 - initial authoring
+# Feb 12, 2013 - use local vars, added fix_jailname()
+# Oct 19, 2012 - compatible with jail names with a . in them
+# Apr 22, 2010 - added mergemaster feature
+# Feb 02, 2009 - added support for jail names with - in name (ezjail compat)
+# Sep 27, 2007 - added all target
+# Sep 23, 2007 - added tripwire
+# Sep 18, 2007 - added sudo
+# Sep 16, 2007 - initial authoring
 
 # configurable settings
 JAILBASE="/usr/jails"
-JAILRC="/usr/local/etc/rc.d/ezjail.sh"
+JAILRC="/usr/local/etc/rc.d/ezjail"
 SUDO=''
 
 usage() {
-	echo "   usage: $0 [ jailname ]"
-	echo ""
-	exit
+    echo "   usage: $0 [ jailname ]"
+    echo " "
+    echo " jailname has two special options: all, mergemaster"
+    echo " "
+    echo " all         - consecutively log into each jail "
+    echo " mergemaster - run mergemaster for each jail"
+    echo " "
+    exit
 }
 
 if [ -z $1 ];
@@ -22,29 +31,41 @@ then
     usage
 fi
 
+fix_jailname()
+{
+    # ezjail renames char - to _
+    _jail_fixed=`echo "$1" | sed -e 's/\-/_/g'`
+    _jail_fixed=`echo "$_jail_fixed" | sed -e 's/\./_/g'`
+    echo $_jail_fixed
+}
+
 jail_manage()
 {
-    _jail="$1"
-
-    echo "processing jail $_jail"
+    local _jail="$1"
 
     if [ -z "$_jail" ]; then
         echo " didn't receive the jail name!" && echo
         return
     fi
 
-    _pid="/var/run/jail_${_jail}.id"
+    local _pid="/var/run/jail_${_jail}.id"
 
     if [ ! -f $_pid ]; then
-        echo "    not running: $_jail" && echo
-        return
+        #echo "    no PID found for $_jail" && echo
+        local _jail_fixed=`fix_jailname $1`
+
+        _pid="/var/run/jail_${_jail_fixed}.id"
+        if [ ! -f $_pid ]; then
+            echo "    not running: $_jail" && echo
+            return
+        fi
     fi
 
-    _jail_id=`/usr/bin/head -n1 $_pid`
-    _jexec="/usr/sbin/jexec $_jail_id"
+    local _jail_id=`/usr/bin/head -n1 $_pid`
+    local _jexec="/usr/sbin/jexec $_jail_id"
 
     _mount_ports $_jail
-    _i_mounted=$?
+    local _i_mounted=$?
 
     $SUDO $_jexec su
 
@@ -54,12 +75,31 @@ jail_manage()
     then
         _unmount_ports $_jail
     fi
+
+    check_tripwire $_jail
+}
+
+jail_mergemaster()
+{
+    local _jail="$1"
+
+    if [ -z "$_jail" ]; then
+        echo " didn't receive the jail name!" && echo
+        return
+    fi
+
+    local CMD="mergemaster -FU -D $JAILBASE/$1"
+    echo "$SUDO $CMD"
+    $SUDO $CMD
+
+    echo "all done with $1!"
+    #check_tripwire $_jail
 }
 
 check_tripwire()
 {
-    _jail="$1"
-    _jaildir="$JAILBASE/$_jail"
+    local _jail="$1"
+    local _jaildir="$JAILBASE/$_jail"
 
     if [ ! -d "$_jaildir/var/db/tripwire" ];
     then
@@ -75,8 +115,8 @@ check_tripwire()
     fi
 
     # check email report sending prefs
-    _tw_cfg="$_jaildir/usr/local/etc/tripwire/twcfg.txt"
-    MAIL_VIOL=`$SUDO grep LNOV $_tw_cfg | grep -v true`
+    local _tw_cfg="$_jaildir/usr/local/etc/tripwire/twcfg.txt"
+    local MAIL_VIOL=`$SUDO grep LNOV $_tw_cfg | grep -v true`
 
     if [ -z "$MAIL_VIOL" ]; then
         dialog --yesno "Tripwire is configured to spam you daily. Would you like to only get emails if violations are found?" 7 70
@@ -88,14 +128,14 @@ check_tripwire()
     fi
 
     # run the tripwire check script
-    _pid="/var/run/jail_${_jail}.id"
-    _jail_id=`/usr/bin/head -n1 $_pid`
-    _jexec="/usr/sbin/jexec $_jail_id"
+    local _pid="/var/run/jail_${_jail}.id"
+    local _jail_id=`/usr/bin/head -n1 $_pid`
+    local _jexec="/usr/sbin/jexec $_jail_id"
 
     $SUDO $_jexec /usr/local/sbin/tripwire -m c
 
     # update the database
-    _last_report=`$SUDO /bin/ls $_jaildir/var/db/tripwire/report | tail -n1`
+    local _last_report=`$SUDO /bin/ls $_jaildir/var/db/tripwire/report | tail -n1`
     $SUDO $_jexec /usr/local/sbin/tripwire -m u -a -r $jail_dir/var/db/tripwire/report/$_last_report
 }
 
@@ -109,7 +149,7 @@ check_base()
 
 check_sudo()
 {
-    _uid=`whoami`
+    local _uid=`whoami`
     if [ "$_uid" != 'root' ];
     then
         echo "running as $_uid, using sudo"
@@ -123,26 +163,22 @@ check_sudo()
 
 _mount_ports()
 {
-    _jail="$1"
+    local _jail="$1"
+    local _jail_fixed=`fix_jailname $1`
+    local _ports_dir="$JAILBASE/$_jail/usr/ports"
 
-    # there's a good chance ezjail renamed any - to _ chars
-    _fixed_jail=`echo "$_jail" | sed -e 's/_/-/g'`
-
-    _ports_dir="$JAILBASE/$_fixed_jail/usr/ports"
-
-    if [ -f "$_ports_dir/INDEX" ]; 
+    if [ -f "$_ports_dir/Makefile" ];
     then
         echo "    already mounted: $JAILBASE/$_jail/usr/ports"
         return 0
-    else 
-        _fstab_dir=`grep ports /etc/fstab.$_jail | cut -f2 -d" "`
+    else
+        _fstab_dir=`grep ports /etc/fstab.$_jail_fixed | cut -f2 -d" "`
 
         if [ ! -z "$_fstab" ];
         then
-            echo "    mount -F /etc/fstab.$_jail $_fstab_dir"
-            $SUDO mount -F /etc/fstab.$_jail $_fstab_dir
+            echo "    mount -F /etc/fstab.$_jail_fixed $_fstab_dir"
+            $SUDO mount -F /etc/fstab.$_jail_fixed $_fstab_dir
         else
-
             echo "    mount_nullfs /usr/ports $_ports_dir"
             $SUDO mount_nullfs /usr/ports $_ports_dir
         fi
@@ -152,21 +188,16 @@ _mount_ports()
 
 _unmount_ports()
 {
-    _jail="$1"
+    local _jail="$1"
+    local _ports_dir="$JAILBASE/$_jail/usr/ports"
 
-    # there's a good chance ezjail renamed any - to _ chars
-    _fixed_jail=`echo "$_jail" | sed -e 's/_/-/g'`
-
-    _ports_dir="$JAILBASE/$_fixed_jail/usr/ports"
-    #echo "    ports_dir: $_ports_dir"
-
-    if [ -f "$_ports_dir/INDEX" ]; 
+    if [ -f "$_ports_dir/Makefile" ];
     then
         echo "    /sbin/umount $_ports_dir"
         $SUDO /sbin/umount $_ports_dir
     fi
 
-    if [ -f "$_ports_dir/INDEX" ]; then
+    if [ -f "$_ports_dir/Makefile" ]; then
         echo "    ERR: failed to unmount $_ports_dir"
         return 0
     else
@@ -179,20 +210,27 @@ _unmount_ports()
 check_base
 check_sudo
 
-for _j in `ls /usr/local/etc/ezjail`;
-do
-    case "$1" in
-        "$_j"    )
-            jail_manage $_j
-            check_tripwire $_j
-        ;;
-        "all"   )
+case "$1" in
+    "all"   )
+        for _j in `grep _hostname /usr/local/etc/ezjail/* | cut -f2 -d'=' | sed -e 's/"//g'`;
+        do
             echo "Entering jail $_j"
             sleep 2
             jail_manage $_j
-            check_tripwire $_j
-        ;;
-    esac
-done
+        done
+    ;;
+    "mergemaster"   )
+        for _j in `grep _hostname /usr/local/etc/ezjail/* | cut -f2 -d'=' | sed -e 's/"//g'`;
+        do
+            echo "Doing mergemaster for jail $_j"
+            sleep 2
+            jail_mergemaster $_j
+        done
+    ;;
+    *)
+        echo "Entering jail $1"
+        jail_manage $1
+    ;;
+esac
 
 exit;
